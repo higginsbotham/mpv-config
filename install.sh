18f7e1f1cd851197cca79f64fdf75d312794835f
Higginsbotham <higginsbotham@users.noreply.github.com>
2017-06-10 15:57:58 -0400

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 brew reinstall --without-harfbuzz libass
-brew reinstall --with-rtmpdump --with-openssl --with-libass --with-libbs2b --with-rubberband ffmpeg
+brew reinstall --with-rtmpdump --with-openssl --with-libass --with-libbs2b --with-rubberband --with-tesseract --with-fdk-aac --with-libbs2b --with-libsoxr --with-libvidstab --with-libvorbis --with-libvpx --with-opus --with-sdl2 --with-tools --with-x265 ffmpeg
 brew reinstall --with-vapoursynth --with-rubberband --with-libarchive --with-bundle mpv
 brew reinstall mvtools
 brew reinstall ffms2
